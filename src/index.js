c4b7734931ad317bc90b4e7bce8467a4e7c38ab5
zhulym <zhulym@mail.ru>
2021-09-03 16:27:47 +0300

    feat: last attempt

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 module.exports = function solveSudoku(matrix) {
   const DIGITS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+
+  let copyMatrix;
+  let matrixSolved = false;
+  let step = 1;
+
   function checkMatrix() {
-    let checkSolve = [];
-    for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; j < matrix.length; j++) {
-        if (matrix[i][j] === 0) {
-          checkSolve.push('Matrix isn`t solve');
+    let checkingArr = null;
+    for (let i = 0; i < copyMatrix.length; i++) {
+      for (let j = 0; j < copyMatrix.length; j++) {
+        if (copyMatrix[i][j] === 0) {
+          checkingArr += 1;
         }
       }
     }
-    if (!checkSolve) return matrix;
+    if (!checkingArr) {
+      matrixSolved = true
+    }
   }
 
   function getSquare(row, col) {
@@ -30,14 +37,14 @@ module.exports = function solveSudoku(matrix) {
       case (6 <= row && row <= 8) && (col <= 2):
         fillSquare(6, 0); break;
 
-      case (2 <= row) && (3 <= col && col <= 5):
+      case (row <= 2) && (3 <= col && col <= 5):
         fillSquare(0, 3); break;
       case (3 <= row && row <= 5) && (3 <= col && col <= 5):
         fillSquare(3, 3); break;
       case (6 <= row && row <= 8) && (3 <= col && col <= 5):
         fillSquare(6, 3); break;
 
-      case (2 <= row) && (6 <= col && col <= 8):
+      case (row <= 2) && (6 <= col && col <= 8):
         fillSquare(0, 6); break;
       case (3 <= row && row <= 5) && (6 <= col && col <= 8):
         fillSquare(3, 6); break;
@@ -49,25 +56,44 @@ module.exports = function solveSudoku(matrix) {
     return square;
   }
 
-  for (let i = 0; i < matrix.length; i++) {
-    checkMatrix();
-    for (let j = 0; j < matrix.length; j++) {
-      if (matrix[i][j] === 0) {
-        let horizontal = matrix[i];
-        let vertical = [];
+  function getSolution(nextStep) {
 
-        matrix.forEach(subArr => vertical.push(subArr[j]));
+    for (let i = 0; i < copyMatrix.length; i++) {
+      let isChange = false;
 
-        for (let k = 1; k <= DIGITS.length; k++) {
-          if (!(horizontal.includes(k) && horizontal.includes(k))) {
-            matrix[i][j] = k;
-            break;
+      for (let j = 0; j < copyMatrix.length; j++) {
+
+        if (isChange) {
+          i = -1;
+          j = -1;
+          break;
+        }
+
+        if (copyMatrix[i][j] === 0) {
+          let horizontal = copyMatrix[i];
+          let vertical = [];
+          let currentSquare = getSquare(i, j);
+
+          copyMatrix.forEach(subArr => vertical.push(subArr[j]));
+
+          for (let k = nextStep; k <= DIGITS.length; k++) {
+            if ((!horizontal.includes(k) && !vertical.includes(k) && !currentSquare.includes(k))) {
+              copyMatrix[i][j] = k;
+              isChange = true;
+              break;
+            }
           }
         }
       }
     }
-    // i = -1;
+    checkMatrix();
   }
 
-  return matrix;
+  while (!matrixSolved && step <= 9) {
+    copyMatrix = matrix.map(el => [...el]);
+    getSolution(step);
+    step++;
+  }
+
+  return copyMatrix;
 }
